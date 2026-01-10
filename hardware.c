98d02ffe51769479f8c7f7825b326c92252d3669
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-09 19:08:43 -0500

    Proper dual-axis joystick test

--- hardware.c
+++ hardware.c
@@ -148,9 +148,12 @@ void fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t c) {
 	}
 }
 
-uint16_t get_x1024() {
-
+inline uint16_t get_x1024() {
 	return ADC_read(0);
+}
+
+inline uint16_t get_y1024() {
+	return ADC_read(1);
 }
 
 void sleep() {
--- main.c
+++ main.c
@@ -4,8 +4,19 @@ int main() {
 
 	init_hardware();
 
+	uint16_t prev_x = 0;
+	uint16_t prev_y = 0;
+
 	while (1) {
-		fill_rect(0, 0, 128, 160, get_x1024() << 1);
+
+		// clear
+		fill_rect(prev_x, prev_y, 10, 10, 0x0000);
+
+		prev_x = get_x1024() >> 4;
+		prev_y = get_y1024() >> 4;
+
+		fill_rect(prev_x, prev_y, 10, 10, 0xF448);
+
 		sleep();
 	}
 }
